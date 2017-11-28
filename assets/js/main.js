d8dc7bf0fabd98a994c01a01a2696114b405f547
Lewis Menelaws <lewismenelaws@Lewiss-MacBook-Air.local>
2017-11-27 20:26:25 -0500

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+TweenMax.from("#front-street", 2, {x:-600, ease:Power4.easeOut});
+
+TweenMax.from("#real-estate-marketing", 2, {x:600, ease:Power4.easeOut, delay: 0.2});
+
+TweenMax.from("#Rectangle-6", 2, {y:600, ease:Power4.easeOut});
+TweenMax.from("#Rectangle-6-Copy", 2, {y:600, ease:Power4.easeOut, delay: 0.2});
+TweenMax.from("#Rectangle-6-Copy-2", 2, {y:600, ease:Power4.easeOut, delay: 0.4});
+TweenMax.from("#Rectangle-6-Copy-3", 2, {y:600, ease:Power4.easeOut, delay: 0.6});
+TweenMax.from("#Rectangle-6-Copy-4", 2, {y:600, ease:Power4.easeOut, delay: 0.8});
